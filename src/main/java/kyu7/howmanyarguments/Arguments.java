583596da53ab2687283bd2dabba8a34b5f7a7623
Luke Garrigan <lukegarrigan8@gmail.com>
2019-06-17 20:37:38 +0100

    That kata was pretty pointless, just wanted to create the 7ku package, but if questions are that shoddy that'll be the last.

---
+++ src/main/java/kyu7/howmanyarguments/Arguments.java
@@ -0,0 +1,8 @@
+package kyu7.howmanyarguments;
+
+public class Arguments {
+
+    public static int countArgs(Object... args) {
+        return args.length;
+    }
+}
---
+++ src/test/java/kyu7/howmanyarguments/ArgumentsTest.java
@@ -0,0 +1,24 @@
+package kyu7.howmanyarguments;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ArgumentsTest {
+
+
+    @Test
+    public void test1Argument() {
+
+        assertEquals(1, Arguments.countArgs("11"));
+    }
+
+    @Test
+    public void test0Argument() {
+
+        assertEquals(0, Arguments.countArgs());
+    }
+
+
+
+}
